b11937b27c26ca81030ddab3c13780c9188d906a
Yousef Elserwey <youseef.elserwey@gmail.com>
2025-05-01 15:39:35 +0300

    only support ios platform

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Stinsen",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v10_15),
-        .tvOS(.v16),
-        .watchOS(.v9)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "Stinsen", targets: ["Stinsen"])
